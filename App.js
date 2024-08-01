17ec5327ba78762e2c205993fad2900a8623cc25
Tanjil <tanjilkh@gmail.com>
2024-08-01 11:38:48 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
 import {API_KEY} from '@env';
 
 export default function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [weather, setWeather] = useState(null);
+  const getWeather = async() => {
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${API_KEY}`
+      const response = await fetch(url);
+      const json = await response.json();
+      setWeather(json.weather[0]);
+    } catch(error) {
+      console.log(error)
+    } finally {
+      setLoading(false);
+    }
+  };
 
+  useEffect(() => {
+    getWeather();
+  }, [])
+  
   return (
     <View style={styles.container}>
-      <Text>Teste!</Text>
-      <Text>My API is {API_KEY}</Text>
-      <StatusBar style="auto" />
+      {isLoading ? (
+        <ActivityIndicator/>
+      ) : (
+        weather ? (
+          <View style={styles.itemContainer}>
+            <Text style={styles.item}>
+              {weather.main === 'Clear' ? 'Não é preciso guarda chuva! ❌': 'Traz guarda chuva! ☂️'}
+            </Text>
+          </View>
+        ) : null
+      )}
     </View>
   );
 }
@@ -20,4 +47,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  item: {
+    fontSize: 18,
+  },
+
+  itemContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 8,
+  },
+
+  list: {
+    flexGrow: 1,
+    justifyContent: 'center',
+  }
+
+
+ 
 });
