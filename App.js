54d9225840d0fb26969a872438f1be05700e76c8
Tanjil <tanjilkh@gmail.com>
2024-08-01 11:03:27 +0100

    Update App.js
    Pegar a api key

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {API_KEY} from '@env';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Teste!</Text>
+      <Text>My API is {API_KEY}</Text>
       <StatusBar style="auto" />
     </View>
   );
